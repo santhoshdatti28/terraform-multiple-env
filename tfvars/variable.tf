96d0cc83b90a2533ae7a02a5615963499847fc23
sample <sample@gmail.com>
2025-03-13 17:26:50 +0530

    multiple-env

--- tfvars/variable.tf
+++ tfvars/variable.tf
@@ -9,3 +9,11 @@ variable "environment"{
 variable "instances"{
     default= ["mysql","frontend","backend"]
 }
+
+variable "zone_id"{
+    default= "Z04263705DOX3TZF5K54"
+}
+
+variable "domain_name"{
+    default= "santhoshdatti.online"
+}
